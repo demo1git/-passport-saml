c964ab1c9b4c255ea4d512cb2ca6df892db8d7bc
Anand Joglekar <anandjo@cybage.com>
2017-12-06 12:40:10 +0530

    onelogin IdP

--- config/config.js
+++ config/config.js
@@ -8,7 +8,7 @@ module.exports = {
       strategy: 'saml',
       saml: {
         path: process.env.SAML_PATH || '/login/callback',
-        entryPoint: process.env.SAML_ENTRY_POINT || 'https://openidp.feide.no/simplesaml/saml2/idp/SSOService.php',
+        entryPoint: process.env.SAML_ENTRY_POINT || 'https://cybage-sso-demo-dev.onelogin.com/trust/saml2/http-post/sso/728295',
         issuer: 'passport-saml',
         cert: process.env.SAML_CERT || null
       }
